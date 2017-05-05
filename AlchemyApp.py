8a728a1791f2fa2e926c5c3b7efb5a6bab257bf5
yKF6600 <yxs1112003>
2017-05-05 20:28:30 +0800

    login 逻辑

--- AlchemyApp.py
+++ AlchemyApp.py
@@ -1,41 +1,11 @@
-from flask import Flask, request
-from flask_pymongo import PyMongo
-from flask_restful import abort
+f = None
+try:
+    f = open('WorkList/To_Do_List.todo', 'r')
+    to_do_list = f.read()
 
-from util import CommonUtil
+finally:
+    if f:
+        f.close()
 
-logger = CommonUtil.logger
-app = Flask(__name__)
-app.config.update(
-    MONGO_URI='mongodb://172.27.12.67:27017/sun',
-)
-
-mongo = PyMongo(app)
-
-
-@app.route('/')
-def index():
-    return "Hello Flask"
-
-
-@app.route('/addUser', methods=['GET', 'POST'])
-def add_user():
-    # user = {'name': 'Michael', 'age': 18, 'scores': [{'course': 'Math', 'score': 76}]}
-    post_data = request.get_json()
-    if post_data['username'] is None or post_data['password'] is None:
-        abort(400)
-    # if User.query.filter_by(username=username).first() is not None:
-    #     abort(400)  # existing user
-    mongo.db['users'].insert_one(post_data)
-    return "Inserted Successfully!"
-
-
-@app.route('/query', methods=['GET'])
-def query_user():
-    username = request.args.get('username')
-    if username is not None:
-        logger.info(username)
-
-
-if __name__ == '__main__':
-    app = app.run(debug=True)
+with open('WorkList/To_Do_List.todo', 'w') as f:
+    f.write('Hello, world!')
